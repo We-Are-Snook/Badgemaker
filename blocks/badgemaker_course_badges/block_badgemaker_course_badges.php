61f61af933996f2a18d1f060ad572b2db92bd212
maverick808 <mavgit@marekbell.com>
2017-01-24 19:01:10 +0000

    Added logo to bottom of course_badges block

--- blocks/badgemaker_course_badges/block_badgemaker_course_badges.php
+++ blocks/badgemaker_course_badges/block_badgemaker_course_badges.php
@@ -98,6 +98,16 @@ class block_badgemaker_course_badges extends block_base {
 
         $this->content->text .= $output->print_combined_overview_list($earned, $earnable, 100, 'center', null, null, false);
 
+        $this->content->text .= html_writer::start_div('course_badges_logo', array('align' => 'center'));
+        $liblink = local_badgemaker_libraryPageURL();
+        $logolink = html_writer::start_tag('a', array('href' => $liblink));
+        $ls = local_badgemaker_logo_source();
+        $img = html_writer::empty_tag('img', array('src' => $ls, 'width' => '50px'));
+        $logolink .= $img;
+        $logolink .= html_writer::end_tag('a');
+        $this->content->text .= $logolink;
+        $this->content->text .= html_writer::end_div('course_badges_logo');
+
         return $this->content;
 	}
 }
