b1cc6505c41de2cbd2faa0d8e7939ed53a2a3412
maverick808 <mavgit@marekbell.com>
2017-02-01 12:14:01 +0000

    Rename of user_overview block

---
+++ blocks/badgemaker_overview/lang/en/block_badgemaker_overview.php
@@ -0,0 +1,15 @@
+<?php
+
+$string['pluginname'] = 'BadgeMaker - Overview';
+$string['nothingtodisplay'] = 'There is no badge information to display';
+$string['badges:addinstance'] = 'Add a new BadgeMaker Overview block';
+$string['badges:myaddinstance'] = 'Add a new BadgeMaker Overview block to Dashboard';
+$string['site_badges'] = ' site badges available';
+$string['course_badges'] = ' course badges available';
+$string['all_badges'] = ' badges available';
+$string['awarded'] = ' badges awarded';
+$string['title'] = 'Badges Overview';
+$string['footer'] = 'Badges assisted by Badgemaker';
+$string['you_have_earned'] = 'You have earned';
+$string['badges'] = 'badges';
+$string['at'] = 'at';
